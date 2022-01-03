4cf7748438987a985bb0f10ebf10f28c420465a0
Fabian Mastenbroek <mail.fabianm@gmail.com>
2022-01-03 14:20:10 +0100

    Shim ubnt_sta_ht interface
    This change creates a shim for the ubnt_sta_ht character device created
    by ubnt_common. We do not implement the functionality of the interface,
    and only return the not implemented error.

--- ubnt-common-shim.c
+++ ubnt-common-shim.c
@@ -13,6 +13,8 @@
 
 #include <linux/err.h>
 #include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/device.h>
 
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Fabian Mastenbroek <mail.fabianm@gmail.com>");
@@ -20,8 +22,38 @@ MODULE_DESCRIPTION("ubnt-common shim for the UniFi Dream Machine (Pro)");
 MODULE_VERSION("1.0");
 MODULE_ALIAS("ubnt_common");
 
+static long ubnt_sta_ht_ioctl(struct file *file, unsigned req, unsigned long arg)
+{
+	/* Unsupported */
+	return -ENOSYS;
+}
+
+static int ubnt_sta_ht_maj;
+static const struct file_operations ubnt_sta_ht_fops = {
+	.owner = THIS_MODULE,
+	.unlocked_ioctl = ubnt_sta_ht_ioctl,
+};
+
+int ubnt_sta_ht_init(void)
+{
+	ubnt_sta_ht_maj = register_chrdev(0, "ubnt_sta_ht", &ubnt_sta_ht_fops);
+	if (ubnt_sta_ht_maj < 0) {
+		pr_err("Unable to register char device:%s\n", "ubnt_sta_ht");
+		return ubnt_sta_ht_maj;
+	}
+	return 0;
+}
+
+void ubnt_sta_ht_exit(void)
+{}
+
 static int __init ubnt_common_init(void)
 {
+	int err = ubnt_sta_ht_init();
+	if (err < 0) {
+		pr_err("Failed to init sta_ht (%d)", err);
+		return err;
+	}
 	return 0;
 }
 
@@ -30,5 +62,6 @@ module_init(ubnt_common_init);
 static void __exit ubnt_common_exit(void)
 {
 	pr_info("Unloading...\n");
+	ubnt_sta_ht_exit();
 }
 module_exit(ubnt_common_exit);
